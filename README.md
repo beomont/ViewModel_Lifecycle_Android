5c2a19bffe995e8c9c474d889239a8cb05b8e429
beomont <brunolopesg@gmail.com>
2021-07-18 02:10:25 -0300

    Versão 1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# ViewModel e Lifecycle para Melhor Experiência em Aplicativos Android :computer:
+
+Teoria e prática sobre ViewModel e LifeCycle. :wave:
+
+:rocket: :smiley: :mobile_phone_off:
+
---
+++ app/src/main/java/com/beomont/vmodel/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.beomont.vmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    var mContador = MutableLiveData<String>().apply{value = contador.toString() }
+    private var contador: Int = 0
+
+    private fun setmContador (){
+        mContador.value = contador.toString()
+    }
+
+    private fun validaContador() {
+        contador++
+        if(contador > 10){
+            contador = 0
+        }
+        setmContador()
+    }
+
+    fun Contador(){
+        validaContador()
+    }
+}
